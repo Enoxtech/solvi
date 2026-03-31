51236778a36b104434b4130888e63f51bc532f42
Enoch <enoch@seekerpad.app>
2026-03-31 22:45:19 +0100

    debug: strip providers to minimum to isolate crash

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,36 +2,7 @@
 
 import type React from "react"
 import { AuthProvider } from "@/contexts/AuthContext"
-import { WalletProvider } from "@/contexts/WalletContext"
-import { CurrencyProvider } from "@/contexts/CurrencyContext"
-import { NotificationProvider } from "@/contexts/NotificationContext"
-import { PageWrapper } from "@/components/PageWrapper"
-import { FundWalletDialog } from "@/components/FundWalletDialog"
-import { GlobalWidgets } from "@/components/GlobalWidgets"
-import { WalletStateObserver } from "@/components/WalletStateObserver"
-import { BottomNavWrapper } from "@/components/BottomNavWrapper"
-import { FocusVisiblePolyfill } from "@/components/FocusVisiblePolyfill"
-import { Toaster } from "@/components/ui/sonner"
-import { TransactionNotification } from "@/components/TransactionNotification"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <AuthProvider>
-      <WalletProvider>
-        <CurrencyProvider>
-          <NotificationProvider>
-            <PageWrapper>
-              {children}
-              <FundWalletDialog />
-              <GlobalWidgets />
-              <WalletStateObserver />
-              <BottomNavWrapper />
-              <FocusVisiblePolyfill />
-            </PageWrapper>
-            <Toaster />
-          </NotificationProvider>
-        </CurrencyProvider>
-      </WalletProvider>
-    </AuthProvider>
-  )
+  return <AuthProvider>{children}</AuthProvider>
 }
